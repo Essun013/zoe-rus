34b9a946551852b44682c78d186fb2155e509af3
ianchen <Chenf@zoenet.cn>
2016-10-12 16:04:13 +0800

    首页提交

--- js/view/Status/StatusNav.js
+++ js/view/Status/StatusNav.js
@@ -5,20 +5,9 @@
 import React, {Component} from 'react'
 import Status from './Status';
 import Nav from '../../components/Nav/Nav';
-import {Navigator, Text, StatusBar, View, TouchableOpacity, StyleSheet, Alert} from 'react-native';
 
 export default class FindNav extends Component {
-    constructor(props) {
-        super(props);
-
-        this.testTitleFn = this.testTitleFn.bind(this);
-    }
-
-    testTitleFn() {
-        return <Text>123</Text>
-    }
-
     render() {
-        return <Nav route={{component: Status, title: this.testTitleFn}} barStyle={{backgroundColor: '#ff5884'}}/>
+        return <Nav route={{component: Status, title: '状态选择'}} barStyle={{backgroundColor: '#ff5884'}}/>
     }
 }
