786d24abbc306bbf76bb2439fcb8c451b52c999b
sea35 <sea35@163.com>
2016-11-04 16:16:02 +0800

    新建我基本信息

--- js/actions/me/me.js
+++ js/actions/me/me.js
@@ -4,6 +4,7 @@
 
 const LOGIN_SYS = 'LOGIN_SYS';
 const SET_USER = 'SET_USER';
+const SET_CHILDBIRTH ='SET_CHILDBIRTH';
 
 function loginSys(loginState) {
     return {
@@ -20,9 +21,18 @@ function setUser(user) {
     }
 }
 
+function setChildbirth(childbirth) {
+    return {
+        type:SET_CHILDBIRTH,
+        childbirth
+    }
+}
+
 module.exports ={
     loginSys,
     setUser,
+    setChildbirth,
     LOGIN_SYS,
-    SET_USER
+    SET_USER,
+    SET_CHILDBIRTH
 }
--- js/reducers/me/me.js
+++ js/reducers/me/me.js
@@ -2,7 +2,7 @@
  * Created by sea35 on 2016/10/17.
  */
 import React from 'react';
-import {LOGIN_SYS, SET_USER} from '../../actions/me/me';
+import {LOGIN_SYS, SET_USER,SET_CHILDBIRTH} from '../../actions/me/me';
 
 const initialState = {
     user:{},
@@ -18,6 +18,10 @@ function editMe(state = initialState, action) {
         case SET_USER:
             return Object.assign({}, state, {
                 user:action.user
+            });
+        case SET_CHILDBIRTH:
+            return Object.assign({}, state, {
+                childbirth:action.childbirth
             });
         default:
             return state;
